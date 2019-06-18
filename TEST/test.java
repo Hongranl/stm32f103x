4abc40095ae309891183e67513ad77eeaee4cb29
ranhongliang <1072767772@qq.com>
2019-06-18 09:55:25 +0800

    test

---
+++ TEST/test.java
@@ -0,0 +1,23 @@
+ public class test{
+         public static void main(String[] args){
+        System.out.println("Hello World!");
+        byte[] b={0x41,0xf5,0x1e,0xb6};
+
+        byte2float(b,0);
+
+        
+
+    }
+    public static float byte2float(byte[] b, int index) {
+        int l;
+        l = b[index + 0];
+        l &= 0xff;
+        l |= ((long) b[index + 1] << 8);
+        l &= 0xffff;
+        l |= ((long) b[index + 2] << 16);
+        l &= 0xffffff;
+        l |= ((long) b[index + 3] << 24);
+        return Float.intBitsToFloat(l);
+    }
+ }
+ 
