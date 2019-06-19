01d5430ed72ceb7e5255992b29224ec0ae188b74
ranhongliang <1072767772@qq.com>
2019-06-19 17:49:51 +0800

    add readme

--- Relay_8-power-Modle(八路继电器和智能电表)/User/Main.c
+++ Relay_8-power-Modle(八路继电器和智能电表)/User/Main.c
@@ -1,31 +1,38 @@
-/*----------------------------------------------------------------------------
- * CMSIS-RTOS 'main' function template
+/*---------------------------------------------------------------------------
+ *
+ * Copyright (C),2014-2019, guoshun Tech. Co., Ltd.
+ *
+ * @Project:    智能实训台项目
+ * @Version:    V 0.2 
+ * @Module:     Main
+ * @Author:     RanHongLiang
+ * @Date:       2019-06-19 17:48:35
+ * @Description: 
+ *————CMSIS-RTOS
  *---------------------------------------------------------------------------*/
-#define osObjectsPublic                    		 // define objects in main module
-#include "includes.h"//文件内包括用于内核激活，以及Bsp初始化和不可中断进程启动；
 
-int main (void const *argument)
+#define osObjectsPublic // define objects in main module
+#include "includes.h"   //文件内包括用于内核激活，以及Bsp初始化和不可中断进程启动；
+
+int main(void const *argument)
 {
-	
-	osKernelInitialize ();                    	// initialize CMSIS-RTOS
-	
+
+	osKernelInitialize(); // initialize CMSIS-RTOS
+
 	// initialize peripherals here
 
 	// create 'thread' functions that start executing,
 	// example: tid_name = osThreadCreate (osThread(name), NULL);
-	
-		BSP_Init();////文件内包括延时函数初始化及Debug串口输出初始化；
-		
-		wirelessThread_Active(1);
-		
-		MoudleDEC_Init();//模块检测进程
-		
-	
-		
-		
-		//tipsLEDActive();//led提示灯及无源蜂鸣器的驱动函数；
 
-		keyMboardActive();//底板按键检测进程函数及其按键回调函数；
-	
-	osKernelStart ();                         	// start thread execution 
+	BSP_Init(); ////文件内包括延时函数初始化及Debug串口输出初始化；
+
+	wirelessThread_Active(1);
+
+	MoudleDEC_Init(); //模块检测进程
+
+	// tipsLEDActive();//led提示灯及无源蜂鸣器的驱动函数；
+
+	keyMboardActive(); //底板按键检测进程函数及其按键回调函数；
+
+	osKernelStart(); // start thread execution
 }
