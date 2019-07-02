457a3e70ebb35249a1a3f333d8ce5feb3534fccb
ranhongliang <1072767772@qq.com>
2019-07-02 11:54:50 +0800

    火焰传感器--注释

--- MBoard_project/Module/Fire_MS/fireMS.h
+++ MBoard_project/Module/Fire_MS/fireMS.h
@@ -1,3 +1,16 @@
+/*---------------------------------------------------------------------------
+ *
+ * Copyright (C),2014-2019, guoshun Tech. Co., Ltd.
+ *
+ * @Project:    智能实训台项目
+ * @Version:    V 0.2 
+ * @Module:     fireMS
+ * @Author:     RanHongLiang
+ * @Date:       2019-07-02 11:54:12
+ * @Description: 
+ *————火焰传感器头文件
+ *---------------------------------------------------------------------------*/
+
 #ifndef FIRE_MS_H
 #define FIRE_MS_H
 
@@ -11,22 +24,22 @@
 
 #include "debugUart.h"
 
-#define FIRE_DATA	PBin(8);
+#define FIRE_DATA PBin(8);
 
-typedef struct{
+typedef struct
+{
 
 	bool VAL;
-}fireMS_MEAS;
+} fireMS_MEAS;
 
-extern osThreadId 	 tid_fireMS_Thread;
-extern osPoolId  	 fireMS_pool;
-extern osMessageQId  MsgBox_fireMS;
-extern osMessageQId  MsgBox_MTfireMS;
-extern osMessageQId  MsgBox_DPfireMS;
+extern osThreadId tid_fireMS_Thread;
+extern osPoolId fireMS_pool;
+extern osMessageQId MsgBox_fireMS;
+extern osMessageQId MsgBox_MTfireMS;
+extern osMessageQId MsgBox_DPfireMS;
 
 void fireMS_Init(void);
 void fireMS_Thread(const void *argument);
 void fireMSThread_Active(void);
 
 #endif
-
