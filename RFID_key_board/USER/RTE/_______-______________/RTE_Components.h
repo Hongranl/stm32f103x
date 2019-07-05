cadda5716720756f72dba005875bd2d2094122c7
ranhongliang <1072767772@qq.com>
2019-07-05 14:14:00 +0800

    add “iwdog” - 讲台按键读卡模块

---
+++ RFID_key_board/USER/RTE/_______-______________/RTE_Components.h
@@ -0,0 +1,20 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'GS-X-GRE02' 
+ * Target:  '实训室-讲台按键和读卡' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f10x.h"
+
+
+#endif /* RTE_COMPONENTS_H */
